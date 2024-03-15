1604f60cdc1965c5b30e19da10009b616c19f078
Mitchel <febemitch1@gmail.com>
2024-03-15 02:43:29 +0100

    started group anagrams

---
+++ arrays_and_hashing/group_anagrams/solution.go
@@ -0,0 +1,20 @@
+package groupAnagrams
+
+import (
+    "sort"
+)
+
+func groupAnagrams(strs []string) [][]string {
+    // create a hasmap of string -> string array
+    // loop through strs, and sort each str
+    // check if the sorted str is in the hashmap
+    // if it is, append the str to the existing array
+    // else put it as an array in the map
+    // return the values of the map
+
+    store := make(map[string][]string)
+
+    for _, v := range strs {
+		sort.Sort()
+    }
+}
