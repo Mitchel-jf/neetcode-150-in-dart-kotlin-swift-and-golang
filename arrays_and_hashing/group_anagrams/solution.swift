6bf1e93cf2f711c7b3d717ccace495f4fe6a91b8
Mitchel <febemitch1@gmail.com>
2024-03-22 19:38:46 +0100

    adding comments to swift for group anagrams

--- arrays_and_hashing/group_anagrams/solution.swift
+++ arrays_and_hashing/group_anagrams/solution.swift
@@ -1,5 +1,8 @@
 class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
-        
+        // loop through strs
+        // sort str
+        // create a hashmap and store str
+        // 
     }
 }
