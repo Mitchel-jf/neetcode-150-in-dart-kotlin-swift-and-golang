36a0f87fd12867d2daab8f206a53120ae446c468
Mitchel <febemitch1@gmail.com>
2024-03-23 22:30:35 +0100

    completed documentation steps for swift - group anagrams

--- arrays_and_hashing/group_anagrams/solution.swift
+++ arrays_and_hashing/group_anagrams/solution.swift
@@ -3,6 +3,9 @@ class Solution {
         // loop through strs
         // sort str
         // create a hashmap and store str
-        // 
+        // check if str exists in the hashmap for each index of strs
+        // append str if it exists
+        // save put str in a new list and save it to the hashmap
+        // return the values of the hashmap
     }
 }
